72ac3093971a940ae0961f1276c5b1338a7561e9
kik1m <kikkok5k@gmail.com>
2026-05-20 08:32:40 +0300

    fix: new particals phase ready V3.2

--- src/hooks/useNavbar.js
+++ src/hooks/useNavbar.js
@@ -15,7 +15,7 @@ export const useNavbar = () => {
         const handleScroll = () => {
             setIsScrolled(window.scrollY > 20);
         };
-        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('scroll', handleScroll, { passive: true });
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
